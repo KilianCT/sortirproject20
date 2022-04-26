8ee4efc0ffc4507a5a8c82be6de0754ea37c64d8
ksochard2021 <kilian.sochard.pro5@gmail.com>
2022-04-26 13:35:20 +0200

    nouveau dosier

---
+++ src/Controller/SiteController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Site;
+use App\Form\SiteType;
+use App\Repository\SitesRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/site")
+ */
+class SiteController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_site_index", methods={"GET"})
+     */
+    public function index(SitesRepository $sitesRepository): Response
+    {
+        return $this->render('site/index.html.twig', [
+            'sites' => $sitesRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="app_site_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, SitesRepository $sitesRepository): Response
+    {
+        $site = new Site();
+        $form = $this->createForm(SiteType::class, $site);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $sitesRepository->add($site);
+            return $this->redirectToRoute('app_site_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('site/new.html.twig', [
+            'site' => $site,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_site_show", methods={"GET"})
+     */
+    public function show(Site $site): Response
+    {
+        return $this->render('site/show.html.twig', [
+            'site' => $site,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="app_site_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Site $site, SitesRepository $sitesRepository): Response
+    {
+        $form = $this->createForm(SiteType::class, $site);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $sitesRepository->add($site);
+            return $this->redirectToRoute('app_site_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('site/edit.html.twig', [
+            'site' => $site,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_site_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Site $site, SitesRepository $sitesRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$site->getId(), $request->request->get('_token'))) {
+            $sitesRepository->remove($site);
+        }
+
+        return $this->redirectToRoute('app_site_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/SortieType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Sortie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('dateHeureDebut')
+            ->add('duree')
+            ->add('dateLimiteInscription')
+            ->add('nbInscriptionMax')
+            ->add('infosSortie')
+            ->add('etat')
+            ->add('organisateur')
+            ->add('lieux_no_lieux')
+            ->add('site_no_site')
+            ->add('participant_no_participant')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
