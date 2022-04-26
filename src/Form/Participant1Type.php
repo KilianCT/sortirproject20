4b13742e343955130279fb4c000bec9d4ea42aa8
tbouyer2021 <to.bouyer@laposte.net>
2022-04-26 16:04:42 +0200

    les crud plus les securitée d'identification utilisateur

---
+++ src/Form/Participant1Type.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Participant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Participant1Type extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('roles')
+            ->add('password')
+            ->add('pseudo')
+            ->add('nom')
+            ->add('prenom')
+            ->add('telephone')
+            ->add('administrateur')
+            ->add('actif')
+            ->add('photo_url')
+            ->add('sortie_no_sortie')
+            ->add('site_no_site')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}
