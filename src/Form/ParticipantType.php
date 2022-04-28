39d438eab345bbc7c3a8bf122c8e63686f907bdf
Tatiana NOVIK <novik85000@gmail.com>
2022-04-28 13:37:48 +0200

    formulaire d'ajoute de nouveau participant (avec le Select: Site)

--- src/Form/ParticipantType.php
+++ src/Form/ParticipantType.php
@@ -3,6 +3,14 @@
 namespace App\Form;
 
 use App\Entity\Participant;
+use App\Entity\Site;
+use App\Repository\SitesRepository;
+use ContainerC1vJtMb\getSortieRepositoryService;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\Persistence\ObjectManager;
+use http\Env\Response;
+use phpDocumentor\Reflection\DocBlock\Tags\Method;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -12,38 +20,37 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ParticipantType extends AbstractType
+/**
+ * @extends ServiceEntityRepository<Site>
+ *
+ * @method Site|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Site|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Site[]    findAll()
+ * @method Site[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+
+
 {
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+
         $builder
             ->add('email')
             ->add('pseudo')
             ->add('nom')
             ->add('prenom')
             ->add('telephone')
-            ->add('photo_url')
-            ->add('site_no_site', ChoiceType::class, [
-                'choices'=> [
-                    'Nantes' => true,
-                    'Angers' => false,
-                    ]
+            ->add('photo_url');
 
-            ]);
-
-          $builder->add('password', RepeatedType::class, [
+        $builder->add('password', RepeatedType::class, [
             'type' => PasswordType::class,
             'required' => true,
             'invalid_message' => 'Les mots de passe ne sont pas identique !',
             'options' => ['attr' => ['class' => 'password-field']],
-            'first_options'  => ['label' => 'Mot de passe'],
+            'first_options' => ['label' => 'Mot de passe'],
             'second_options' => ['label' => 'Mot de passe (Confirmation)'],
         ]);
-
-            $builder->add('submit', SubmitType::class, [
-
-
-                'label' =>  $options['type'] === 'create' ? 'Créer' : 'Modifier',
-            ]);
 
 
     }
@@ -56,3 +63,4 @@ class ParticipantType extends AbstractType
         ]);
     }
 }
+
